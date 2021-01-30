6c628806f87e3e8144656a0ffe8c21a21cc26dc6
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 00:37:31 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+// let nom = "mounajaik";
+// console.log(nom);
+// let prenom = "abdellah";
+// console.log(prenom);
+// let ville = prompt ("quelle ville vis-tu?");
+// console.log(ville);
+// alert(ville);
+
+
+// let nom = "mounajaik";
+// let prenom = "abdellah";
+// let villeDepart = "bruxelles";
+// let villeDestination = "nador";
+// alert(`Le voyage de ${villeDepart} vers ${villeDestination} a été enregistré au nom de ${prenom} ${nom}`)
+
+
+// let nom = prompt('un nom?');
+// let prenom = prompt('un prénom?');
+// let depart = prompt('une ville de départ?');
+// let destination = prompt('une ville de déstination ?');
+
+// let phrase = `Le voyage de ${depart} vers ${destination} a été enregistré au nom de ${nom} ${prenom}`;
+// console.log(phrase);
